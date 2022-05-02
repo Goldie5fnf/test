52ac3e82888670bd55fd453f360e3a48a33aeb71
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-05-02 19:27:45 +0300

    Drist

---
+++ source/DristGayState.hx
@@ -0,0 +1,49 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.FlxCamera;
+
+class DristGayState extends MusicBeatState
+{
+  var bg:FlxSprite;
+  var accept:FlxSprite;
+
+    override function create()
+    {
+        super.create();
+
+        bg = new FlxSprite().loadGraphic(Paths.image('dristgayover'));
+        bg.screenCenter();
+        bg.visible = true;
+        add(bg);
+
+        accept = new FlxSprite().loadGraphic(Paths.image('dristaccept'));
+        accept.visible = false;
+        accept.screenCenter = true;
+        add(accept);
+    }
+
+    override function update(elapsed:Float) {
+        if(controls.accept) {
+            bg.visible = false;
+            FlxG.camera.flash(FlxColor.WHITE, 1);
+            accept.visible = true;
+            new FlxTimer().start(0.8, function(accept:FlxTimer))
+             MusicBeatState.switchState(new MainMenuState());
+            }
+         if(controls.back)
+           {
+            flash.system.System.exit(0);
+           }
+        super.update(elapsed);
+      }
+}
