aa69351f40eaba4a62232ad4a678bb80fd575f45
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-05-03 19:13:19 +0300

    Cyka

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -217,53 +217,7 @@ class StoryMenuState extends MusicBeatState
 		{
 			var upP = controls.UI_UP_P;
 			var downP = controls.UI_DOWN_P;
-			if (upP)
-			{
-				changeWeek(-1);
-				//FlxG.sound.play(Paths.sound('scrollMenu'));
-			}
-
-			if (downP)
-			{
-				changeWeek(1);
-				//FlxG.sound.play(Paths.sound('scrollMenu'));
-			}
-
-			if (controls.UI_RIGHT)
-				//rightArrow.animation.play('press')
-			else
-				//rightArrow.animation.play('idle');
-
-			if (controls.UI_LEFT)
-				//leftArrow.animation.play('press');
-			else
-				//leftArrow.animation.play('idle');
-
-			if (controls.UI_RIGHT_P)
-				//changeDifficulty(1);
-			else if (controls.UI_LEFT_P)
-				//changeDifficulty(-1);
-			else if (upP || downP)
-				//changeDifficulty();
-
-			if(FlxG.keys.justPressed.CONTROL #if android || _virtualpad.buttonX.justPressed #end)
-			{
-				#if android
-				removeVirtualPad();
-				#end
-				persistentUpdate = false;
-				openSubState(new GameplayChangersSubstate());
-			}
-			else if(controls.RESET #if android || _virtualpad.buttonY.justPressed #end)
-			{
-				#if android
-				removeVirtualPad();
-				#end
-				persistentUpdate = false;
-				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
-				//FlxG.sound.play(Paths.sound('scrollMenu'));
-			}
-			else if (controls.ACCEPT)
+			if (controls.ACCEPT)
 			{
 				selectWeek();
 			}
