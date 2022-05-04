52f1dfe509d8675fc885d5de4ef57755b8938682
Goldie5fnf <82838084+Goldie5fnf@users.noreply.github.com>
2022-05-04 22:17:27 +0300

    Лалаладв

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -203,7 +203,7 @@ class StoryMenuState extends MusicBeatState
 			PlayState.campaignScore = 0;
 			PlayState.campaignMisses = 0;
 			new FlxTimer().start(1, function(tmr:FlxTimer)
-				LoadingState.loadAndSwitchState(new PlayState(), true);
+				LoadingState.loadAndSwitchState(new PlayState(), true));
 				FreeplayState.destroyFreeplayVocals());
 			}
 		}
